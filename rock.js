ee55a3400f1b47fb21d6cc0cdd35a9c4c48fb5fa
shyam2323232323232 <131328505+shyam2323232323232@users.noreply.github.com>
2024-01-06 01:35:17 +0530

    added startbutton in js

--- rock.js
+++ rock.js
@@ -1,28 +1,65 @@
-const roundselect=document.querySelector("#roundSelect")
-const roundsSelectContainer=document.querySelector("#roundsSelectContainer")
-const errorMessagePara=document.querySelector("#errorMessagePara")
-const popup=document.querySelector("#popup")
-const  closebutton=document.querySelector("#closebutton")
+ roundselect=document.querySelector("#roundselect")
+ roundsSelectContainer=document.querySelector("#roundsSelectContainer")
+ errorMessagePara=document.querySelector("#errorMessagePara")
+ popup=document.querySelector("#popup")
+closebutton=document.querySelector("#closebutton")
 
-const  playButton=document.querySelector("#playButton")
-const  nextRoundButton=document.querySelector("#nextRoundButton")
-const  initButton=document.querySelector("#initButton")
+playButton=document.querySelector("#playButton")
+nextRoundButton=document.querySelector("#nextRoundButton")
+initButton=document.querySelector("#initButton")
 
-const  gameControls=document.querySelectorAll("#gameControls")
-const  userControls=document.querySelectorAll("#userControls")
-const  computerControls=document.querySelectorAll("#computerControls")
+gameControls=document.querySelectorAll("#gameControls")
+userControls=document.querySelectorAll("#userControls")
+computerControls=document.querySelectorAll("#computerControls")
 
-const  roundID=document.querySelectorAll("#roundID")
-const roundspan=roundID.querySelector('span')
+roundID=document.querySelector("#roundID")
+// roundspan=roundID.querySelector("span")
 
-const countDown=document.querySelector("#countDown")
+countDown=document.querySelector("#countDown")
 
-const userScore=document.querySelector("#userScore")
-const userFireWork=document.querySelector("#userFireWork")
+userScore=document.querySelector("#userScore")
+userFireWork=document.querySelector("#userFireWork")
 
-const computerScore=document.querySelector("#computerScore")
-const computerFireWork=document.querySelector("#computerFireWork")
- 
+computerScore=document.querySelector("#computerScore")
+computerFireWork=document.querySelector("#computerFireWork")
+LONG_DELAY=1500
+DELAY=1000
+SHORT_DELAY=300
+isGameStarted=false
+init();
+
+currentROund=undefined
+
+function init(){
+    isGameStarted=false;
+    userScore.innerText="0";
+    computerScore.innerText="0";
+     show([playButton,roundsSelectContainer])
+      hide([countDown,roundID,nextRoundButton,initButton])
+     roundSelect.value="5"   
+}
+function startgame(){
+    if(roundSelect.value===""){
+       disaplayError("Choose number of ROunds")
+    }
+    hide([playButton,roundsSelectContainer])
+    show([countDown,roundID])
+    currentROund=1; 
+    countDown.innerText="4"
+    addclasses([countDown],["animate-[bounce_1s_ease-in-out_infinite]"])
+    triggerCountDown()
+}
+function triggerCountDown(){
+    if(+countDown.innerText>1){
+countDown.innerText=+countDown.innerText-1
+setTimeout(() => {
+   triggerCountDown()
+}, DELAY);
+    }else{
+        countDown.innerText="GO!!!"
+        removeclasses([countDown],["animate-[bounce_1s_ease-in-out_infinite]"])
+    }
+}
 
 function openpopup(){
     // popup.classList.remove("hidden")
@@ -45,4 +82,20 @@ function removeclasses(elements,classes){
     element.classList.remove(className)
         })
     });
-    }
+}
+function hide(elements){
+    elements.forEach(element => {
+        element.classList.add('hidden')
+    });
+}
+function show(elements){
+    elements.forEach(element => {
+        element.classList.remove('hidden')
+    });
+}
+function disaplayError(msg){
+    errorMessagePara.innerText=msg
+        setTimeout(() => {
+            errorMessagePara.innerText=""
+        }, LONG_DELAY);
+}
