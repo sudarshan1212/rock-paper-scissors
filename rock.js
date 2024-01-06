c11a50692ca08ab270dec1dc79b3602ec6f02f77
shyam2323232323232 <131328505+shyam2323232323232@users.noreply.github.com>
2024-01-06 23:46:47 +0530

    update

--- rock.js
+++ rock.js
@@ -13,7 +13,7 @@ userControls=document.querySelectorAll(".userControls")
 computerControls=document.querySelectorAll(".computerControls")
 
 roundID=document.querySelector("#roundID")
-// roundspan=roundID.querySelector("span")
+roundspan=roundID.querySelector("span")
 
 countDown=document.querySelector("#countDown")
 
@@ -23,12 +23,11 @@ userFireWork=document.querySelector("#userFireWork")
 computerScore=document.querySelector("#computerScore")
 computerFireWork=document.querySelector("#computerFireWork")
 LONG_DELAY=1500
-DELAY=1000
+DELAY=10
 SHORT_DELAY=300
 isGameStarted=false
 init();
-
-currentROund=undefined
+currentROund=1
 
 function init(){
     isGameStarted=false;
@@ -36,7 +35,7 @@ function init(){
     computerScore.innerText="0";
      show([playButton,roundsSelectContainer])
       hide([countDown,roundID,nextRoundButton,initButton])
-     roundSelect.value="5"   
+     roundSelect.value="3"   
 }
 function startgame(){
     if(roundSelect.value===""){
@@ -45,14 +44,24 @@ function startgame(){
     }
     hide([playButton,roundsSelectContainer])
     show([countDown,roundID])
+    numberofRound=+roundSelect.value
+    resetcountdown()
     currentROund=1; 
+   
+}
+function nextROund(){
+hide([nextRoundButton])
+show([countDown])
+resetcountdown()
+}
+function resetcountdown(){
     countDown.innerText="4"
     addclasses([countDown],["animate-[bounce_1s_ease-in-out_infinite]"])
     triggerCountDown()
 }
 function triggerCountDown(){
     if(+countDown.innerText>1){
-countDown.innerText=+countDown.innerText-3
+countDown.innerText=+countDown.innerText-1
 setTimeout(() => {
    triggerCountDown()
 }, DELAY);
@@ -62,10 +71,81 @@ setTimeout(() => {
         enable(gameControls)
     }
 }
-function enable(elements){
-    elements.forEach((element) => {
-        element.disabled=false
-    });
+function select(userInput){
+    let computerInput=1
+    // let computerInput=Math.floor(Math.random()*3)+1
+   console.log({userInput,computerInput});  
+        disable(gameControls,SHORT_DELAY)    
+if(userInput===computerInput){
+    countDown.innerText="DRAW"
+}else if((userInput===1 && computerInput===3)||
+         (userInput===2 && computerInput===1)||
+         (userInput===3 && computerInput===2)
+){
+    updateScore(userScore)
+    show([userFireWork])
+    hide([userFireWork],1000)       
+}else{
+    updateScore(computerScore)
+    show([computerFireWork])
+    hide([computerFireWork],1000) 
+    
+}prepareForNextRound()
+
+}
+
+function prepareForNextRound(){
+if(currentROund<numberofRound){
+    setTimeout(() => {
+        currentROund++
+        roundspan.innerText=currentROund
+        show([nextRoundButton])
+    }, 1000);
+}else{
+//Gameover
+const userSoreValue=+this.userScore.innerText
+const computerSoreValue=+this.computerScore.innerText
+if(userSoreValue==computerSoreValue){
+ 
+    countDown.innerText="Game Over. It was Draw"
+}else if(userSoreValue>computerSoreValue){
+    countDown.innerText="Game Over. You Won!"
+}else{
+    countDown.innerText="Game Over. You Lost"
+}
+show([initButton])
+}
+
+}
+
+
+
+
+
+
+function updateScore(element){
+    countDown.innerText=""
+    element.innerText= +element.innerText+1
+}
+
+
+
+function enable(elements,delay){
+    setTimeout(() => {
+        elements.forEach((element) => {
+            element.disabled=false
+        });
+    }, delay);
+
+
+}
+function disable(elements,delay){
+    setTimeout(() => {
+        elements.forEach((element) => {
+            element.disabled=true
+        });
+    }, delay);
+  
 
 }
 
@@ -91,10 +171,13 @@ function removeclasses(elements,classes){
         })
     });
 }
-function hide(elements){
-    elements.forEach(element => {
-        element.classList.add('hidden')
-    });
+function hide(elements,delay){
+    setTimeout(() => {
+        elements.forEach(element => {
+            element.classList.add('hidden')
+        });
+    }, delay?delay:0);
+   
 }
 function show(elements){
     elements.forEach(element => {
